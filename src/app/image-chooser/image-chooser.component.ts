6d5999ac45e34e25727dd29ed57eb5e384e02308
kazors <jerome.gerard02@outlook.fr>
2018-12-18 20:27:41 +0100

    oui

---
+++ src/app/image-chooser/image-chooser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-chooser',
+  templateUrl: './image-chooser.component.html',
+  styleUrls: ['./image-chooser.component.css']
+})
+export class ImageChooserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
