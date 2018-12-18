796689b0743f5cbad1e9a384d9735001a99b97ae
kazors <jerome.gerard02@outlook.fr>
2018-12-18 21:54:40 +0100

    test add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageChooserComponent } from './image-chooser/image-chooser.component';
-
+import {HttpClientModule, HttpHeaders}from '@angular/common/http'
+import {Observable} from 'rxjs/Rx';
 @NgModule({
   declarations: [
     AppComponent,
-    ImageChooserComponent
+    ImageChooserComponent,
+    
   ],
   imports: [
+    HttpClientModule,
+    
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/image-chooser/image-chooser.component.ts
+++ src/app/image-chooser/image-chooser.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpHeaders,  HttpClient} from '@angular/common/http';
+import  'rxjs/Rx';
+import {Injectable} from '@angular/core';
+let testheader: HttpHeaders= new HttpHeaders() ;
 @Component({
   selector: 'app-image-chooser',
   templateUrl: './image-chooser.component.html',
   styleUrls: ['./image-chooser.component.css']
 })
-export class ImageChooserComponent implements OnInit {
 
-  constructor() { }
+
+export class ImageChooserComponent implements OnInit {
+  constructor(private http:HttpClient){
+    testheader.set('oui','test')
+  }
+
 
   ngOnInit() {
   }
+  
 
+  voteTad(): void{
+     this.http.get('localhost:4200/bob', {headers: testheader}).map((response: Response) => {
+      console.log(response.json());
+      response.json();
+    });
+
+  }
+
+
+  voteBob(){
+    return this.http.get('localhost:4200/tad', {headers: testheader} )
+  
+}
 }
